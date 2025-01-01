d187d38329d8b04d334123a7933880bc0e847672
litetex <40789489+litetex@users.noreply.github.com>
2025-01-01 23:30:15 +0100

    Update repopick data

--- user/userscripts/post-envsetup-inline.sh
+++ user/userscripts/post-envsetup-inline.sh
@@ -1,10 +1,8 @@
+# See also https://github.com/AndyCGYan/lineage_build_unified/blob/lineage-20-td/buildbot_unified.sh
+
 echo "Executing repopick"
-repopick 321337 -f # Deprioritize important developer notifications
-repopick 321338 -f # Allow disabling important developer notifications
-repopick 321339 -f # Allow disabling USB notifications
-repopick 340916 -f # SystemUI: add burnIn protection
-repopick 342860 -f # codec2: Use numClientBuffers to control the pipeline
-repopick 342861 -f # CCodec: Control the inputs to avoid pipeline overflow
-repopick 342862 -f # [WA] Codec2: queue a empty work to HAL to wake up allocation thread
-repopick 342863 -f # CCodec: Use pipelineRoom only for HW decoder
-repopick 342864 -f # codec2: Change a Info print into Verbose
+repopick -t 13-burnin -r -f
+repopick -t 13-taro-kalama -r -f
+repopick 321337 -r -f # Deprioritize important developer notifications
+repopick 321338 -r -f # Allow disabling important developer notifications
+repopick 321339 -r -f # Allow disabling USB notifications
