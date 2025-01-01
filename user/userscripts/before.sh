9afd8d008be3aa5bd99c24aefc88276fe41f21c8
litetex <40789489+litetex@users.noreply.github.com>
2025-01-01 23:58:27 +0100

    Fix build

--- user/userscripts/before.sh
+++ user/userscripts/before.sh
@@ -1,9 +1,5 @@
 # Ported from https://github.com/Iceows/lineage_build_leaos/blob/lineage-20.0/build.sh
 
-# Was changed in https://github.com/LineageOS/android_vendor_lineage/commit/49b20fd75c747da08697e6db0be1e300b2fff6f6
-# Ported over from https://github.com/Iceows/lineage_build_leaos/commit/18c18ad4b7a5c821a1acb7eab11c06b72344eea0
-export WITH_ADB_INSECURE=true
-
 apply_patches() {
     echo "Applying patch group ${1}"
     bash /root/userscripts/apply_patches.sh ./lineage_patches_leaos/${1}
--- user/userscripts/post-envsetup-inline.sh
+++ user/userscripts/post-envsetup-inline.sh
@@ -1,4 +1,10 @@
+# Ported from https://github.com/Iceows/lineage_build_leaos/blob/lineage-20.0/build.sh
 # See also https://github.com/AndyCGYan/lineage_build_unified/blob/lineage-20-td/buildbot_unified.sh
+
+# Was changed in https://github.com/LineageOS/android_vendor_lineage/commit/49b20fd75c747da08697e6db0be1e300b2fff6f6
+# Ported over from https://github.com/Iceows/lineage_build_leaos/commit/18c18ad4b7a5c821a1acb7eab11c06b72344eea0
+echo "Setting WITH_ADB_INSECURE"
+export WITH_ADB_INSECURE=true
 
 echo "Executing repopick"
 repopick -t 13-burnin -r -f
@@ -6,3 +12,8 @@ repopick -t 13-taro-kalama -r -f
 repopick 321337 -r -f # Deprioritize important developer notifications
 repopick 321338 -r -f # Allow disabling important developer notifications
 repopick 321339 -r -f # Allow disabling USB notifications
+
+# Was changed in https://github.com/LineageOS/android_vendor_lineage/commit/49b20fd75c747da08697e6db0be1e300b2fff6f6
+# Ported over from https://github.com/Iceows/lineage_build_leaos/commit/18c18ad4b7a5c821a1acb7eab11c06b72344eea0
+echo "Setting WITH_ADB_INSECURE"
+export WITH_ADB_INSECURE=true
